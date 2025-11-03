917faaf8a31ff55ec46128063fd9396ba014a454
Siddharth Roy <siddharthroy2708@gmail.com>
2025-11-03 22:20:11 +0530

    Add fault logging and status functions
    Implemented fault logging and active status functions.

--- firmware/Core/Src/fault_table.c
+++ firmware/Core/Src/fault_table.c
@@ -1,9 +1,13 @@
-/* ===================================
- *  fault_table.c
- *  Auto-generated source stub
- *  Project: Industrial_Controller
- * =================================== */
+/* fault_table.c */
+#include "fault_table.h"
+#include "stdio.h"
 
-#include "../Inc/fault_table.h"
+static bool fault_flag = false;
 
-/* TODO: Implement fault_table logic */
+bool Fault_Active(void) { return fault_flag; }
+
+void Fault_Log(uint16_t id)
+{
+    fault_flag = true;
+    printf("FAULT %d occurred!\n", id);
+}
